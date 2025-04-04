ff54046f42e289a56dfc3a6cf68160d8b8f146f1
Cristian E. Guzman Cogolllo <cogolloguzmanesteban@gmail.com>
2025-04-04 16:04:33 -0500

    Creacion del proyecto

---
+++ src/main/java/com/C2KProyect/Api_Rest_C2K/ApiRestC2KApplication.java
@@ -0,0 +1,13 @@
+package com.C2KProyect.Api_Rest_C2K;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestC2KApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestC2KApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/C2KProyect/Api_Rest_C2K/ApiRestC2KApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C2KProyect.Api_Rest_C2K;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestC2KApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
