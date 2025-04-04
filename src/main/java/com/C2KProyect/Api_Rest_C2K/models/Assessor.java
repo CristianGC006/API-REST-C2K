d0b28ac17ed406a62023a8bca595887d43cdf88a
Cristian E. Guzman Cogolllo <cogolloguzmanesteban@gmail.com>
2025-04-04 17:10:13 -0500

    Feature(models):Se agregan las anotaciones correspondientes a los models

--- src/main/java/com/C2KProyect/Api_Rest_C2K/models/Assessor.java
+++ src/main/java/com/C2KProyect/Api_Rest_C2K/models/Assessor.java
@@ -1,4 +1,52 @@
 package com.C2KProyect.Api_Rest_C2K.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "assessor")
 public class Assessor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assessor_id")
+    private Integer assessorId;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name = "phone")
+    private String phone;
+
+    public Assessor() {
+    }
+
+    public Assessor(Integer assessorId, String name, Integer branchId, String phone) {
+        this.assessorId = assessorId;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Integer getAssessorId() {
+        return assessorId;
+    }
+
+    public void setAssessorId(Integer assessorId) {
+        this.assessorId = assessorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/main/java/com/C2KProyect/Api_Rest_C2K/models/Customer.java
+++ src/main/java/com/C2KProyect/Api_Rest_C2K/models/Customer.java
@@ -1,4 +1,74 @@
 package com.C2KProyect.Api_Rest_C2K.models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer")
+    private Integer idCustomer;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "record_date")
+    private Date recordDate;
+
+    public Customer() {
+    }
+
+    public Customer(Integer idCustomer, String name, String phone, String address, Date recordDate) {
+        this.idCustomer = idCustomer;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.recordDate = recordDate;
+    }
+
+    public Integer getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(Integer idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(Date recordDate) {
+        this.recordDate = recordDate;
+    }
 }
+
